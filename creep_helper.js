13a7298fc17654c7a6ed766458de0750154aa8a1
sveri <sveri@sveri.de>
2017-01-11 14:38:00 +0100

    more cleanup

--- creep_helper.js
+++ creep_helper.js
@@ -1,27 +1,11 @@
-var creep_helper = {
-
-    set_path_from_creep_to: function(creep, target) {
-        if (!creep.memory.path) {
-            creep.memory.path = creep.room.findPath(creep.pos, target.pos);
-        }
-    },
-
-    move_or_clear_path: function(creep, target) {
-        creep_helper.set_path_from_creep_to(creep, target);
-
-        if (creep.moveByPath(creep.memory.path) != OK) {
-            creep.memory.path = undefined;
-        }
+Creep.prototype.set_path_from_creep_to = function(target) {
+    if (!this.memory.path) {
+        this.memory.path = creep.room.findPath(this.pos, target.pos);
     }
-
 }
 
-
-module.exports = creep_helper;
-
-
 Creep.prototype.move_or_clear_path = function(target) {
-    creep_helper.set_path_from_creep_to(this, target);
+    this.set_path_from_creep_to(this, target);
 
     if (this.moveByPath(this.memory.path) != OK) {
         this.memory.path = undefined;
